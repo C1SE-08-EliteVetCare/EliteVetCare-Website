32d61e9338e7dd317557e55137f5159288044bd8
quangquoc154 <quangquoc1542002@gmail.com>
2023-09-21 02:00:18 +0700

    Configure directories and install libraries

---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,9 @@
+export const DefaultLayout = () => {
+    return (
+        <>
+            <div className="text-red-500">
+                Default layout
+            </div>
+        </>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primaryColor: '#007AFE',
+                textColor: '#252525',
+            },
+        },
+    },
+    plugins: [],
+}
