137b72f9a9d1fb2fd18caeeb5d8fbf64fc6df097
Phi Hien <nguyenphihien1011@gmail.com>
2024-01-05 18:08:20 +0700

    update Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -30,28 +30,10 @@ const Home = () => {
                     <p className="text-xl font-normal leading-snug mb-2">Dịch vụ phòng khám thú y tại Đà Nẵng - Nơi
                         chúng tôi yêu
                         và chăm sóc cho thú cưng của bạn như là thành viên gia đình thứ tư. </p>
-                    <motion.a
-                        whileHover={{scale: 1.2}}
-                        href="/make-appointment"
-                        className="mt-2 inline-block bg-primaryColor text-white text-lg font-bold px-9 py-1.5 rounded-3xl"
-                    >
-                        Bắt đầu
-                    </motion.a>
                 </div>
             </div>
 
             <CommitmentItem/>
-
-            {/*<div className="grid grid-flow-col gap-6 mb-16 shadow-2xl p-4 bg-indigo-200">*/}
-            {/*  <clinic src={imgHome1} alt="home-1" className="col-span-4" />*/}
-            {/*  <div className="col-span-8 text-center">*/}
-            {/*    <h1 className="text-3xl font-normal leading-tight">Chúng tôi đồng hành cùng tình yêu với thú cưng của bạn</h1>*/}
-            {/*    <p className="mt-8">Tại đây, chúng tôi cam kết đem lại dịch vụ thú y chất lượng cao, sự quan tâm tận tâm, và*/}
-            {/*      sự*/}
-            {/*      đồng hành đáng tin cậy trong việc bảo vệ sức khỏe và hạnh phúc của thú cưng của bạn. Hãy khám phá thế giới*/}
-            {/*      của chúng tôi và tìm hiểu thêm về cách chúng tôi có thể giúp bạn và thú cưng của bạn</p>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
 
             <div className="relative h-[350px] mt-10 mb-16 drop-shadow-2xl bg-[#E8F0FA] p-4 rounded-2xl">
                 <img
